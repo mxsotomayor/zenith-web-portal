5982392e77991df402f35be36464ac58174e31d2
maxwell <cubanmaxramos@gmail.com>
2025-02-20 09:53:43 -0100

    adding block

--- src/components/PromoAlert/index.tsx
+++ src/components/PromoAlert/index.tsx
@@ -4,17 +4,21 @@ import React from "react";
 
 function PromoAlert() {
   return (
-    <div>
-      <div className="container mx-auto py-8">
+    <div className="bg-white  py-8">
+      <div className="container mx-auto">
         <div className="flex gap-4 items-center p-8 bg-blue-900">
-          <TriangleAlert className="text-white" size="62"/>
+          <TriangleAlert className="text-white" size="62" />
           <div className="text-white text-sm flex-1">
-            <h2 className="text-2xl font-semibold">Protégete de mensajes fraudulentos</h2>
+            <h2 className="text-2xl font-semibold">
+              Protégete de mensajes fraudulentos
+            </h2>
             <p className="text-sm mb-2">
               Revisa de qué se trata este tipo de ataque y mantente a salvo de
               los ciberdelincuentes.
             </p>
-            <Link href="" className="text-blue-300 font-bold">Conoce nuestros consejos</Link>
+            <Link href="" className="text-blue-300 font-bold">
+              Conoce nuestros consejos
+            </Link>
           </div>
           <div className="text-white text-sm">
             <Link href="#" className="underline text-blue-300 font-bold">
